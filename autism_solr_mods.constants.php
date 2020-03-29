91ced8a42f94f25398f4e2fce364461d78808316
Seamus Lee <seamuslee001@gmail.com>
2020-03-30 08:52:14 +1100

    Further work on making the module more generic for transfer to production

--- autism_solr_mods.constants.php
+++ autism_solr_mods.constants.php
@@ -1,3 +1,6 @@
 <?php
 define('SERVICE_LISTING_FIELD', '863');
 define('SERVICES_PROVIDED_FIELD', '866');
+define('EVENT_CHAPTER_FIELD', '325');
+define('CONTACT_CHAPTER_FIELD', '60');
+define('DRUPAL_CHAPTER_FIELD', 'itm_field_chapter_reference');
