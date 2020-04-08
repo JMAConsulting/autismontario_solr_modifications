6498609e30fd5f14844ad01b4cda636ba621e7c0
Seamus Lee <seamus.lee@jmaconsulting.biz>
2020-04-08 04:08:09 -0400

    Update constants following database refresh

--- autism_solr_mods.constants.php
+++ autism_solr_mods.constants.php
@@ -1,6 +1,6 @@
 <?php
-define('SERVICE_LISTING_FIELD', '863');
-define('SERVICES_PROVIDED_FIELD', '866');
+define('SERVICE_LISTING_FIELD', '895');
+define('SERVICES_PROVIDED_FIELD', '897');
 define('EVENT_CHAPTER_FIELD', '325');
 define('CONTACT_CHAPTER_FIELD', '60');
 define('DRUPAL_CHAPTER_FIELD', 'itm_field_chapter_reference');
